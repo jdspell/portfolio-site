4f9b510a2672ddf41865ba8536ac2ae2aa6e7916
jdspell <spelljd15@gmail.com>
2019-05-05 21:14:03 -0400

    created portfolio site

---
+++ JS/particles.js
@@ -0,0 +1,5 @@
+// particlesJS.load('particles-js', 'assets/particles.json', function() {
+//     console.log('callback - particles.js config loaded');
+//   });
+
+particlesJS.load('particles-js', 'particles.json', function() {console.log('particles.json loaded...')});
