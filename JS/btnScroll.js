7ebaa76ce17086c7ad9344b3e4fedc1e849734f6
jdspell <spelljd15@gmail.com>
2019-06-01 09:38:53 -0400

    updated summary for mentor match

--- JS/btnScroll.js
+++ JS/btnScroll.js
@@ -1,5 +1,24 @@
-$('#exploreBtn').click(() => {
-    $('html, body').animate({
-        scrollTop: $('.Projects').offset().top
-    }, 200);
-});
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $(".exploreBtn").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
